49ce245239b6e4f10a773b0ee83bcc1c158edcc0
colby lewis <colewis@umich.edu>
2018-02-08 14:13:47 -0500

    uploading files from local

---
+++ scrapehello.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+
+f = open("hello.html")
+html = f.read()
+soup = BeautifulSoup(html, 'html.parser')
+
+#print(soup.prettify())
+
+
+# searching by tag
+all_list_items = soup.find_all('li')
+all_divs = soup.find_all('div')
+
+# searching by class
+all_goodbye_elements = soup.find_all(class_='goodbye') #class must have an underscore since it is a reserved word in python
+
+# searching by tag AND class
+all_french_list_items = soup.find_all('li', class_='french')
+
+# searching by id
+all_hello_elements = soup.find_all(id='hello-list')
+
+
+#print('list items:', all_list_items)
+#print('------')
+#print('divs:', all_divs)
+#print('------')
+#print('goodbye elements:', all_goodbye_elements)
+#print('------')
+#print('french stuff:', all_french_list_items)
+#print('------')
+#print('hello id elements:', all_hello_elements)
+#print('------')
+
+#print(type(all_list_items[0])) #<class 'bs4.element.Tag'>
+#print('------')
+
+
+#for li in all_list_items: #just gets us the strings within the tags
+#  print(li.string)
+#print('------')
+
+
+#for child in all_hello_elements[0].children:
+#  print(child.string) #None because the first child of our <div> is just the string “We say Hello!”
+#print('------')
+
+
+#find_all way:
+#print('List items within the hello tag')
+hello_list_items = all_hello_elements[0].find_all('li')
+#print (hello_list_items)
+#print('------')
+
+#better way:
+the_hello_element = soup.find(id='hello-list')
+#print(the_hello_element)
+#print('------')
+
+
+img_tag = soup.find('img') #accesses attributes of a tag by treating the tag as a dictionary
+print('The img source:')
+print(img_tag['src'])
+print('------')
